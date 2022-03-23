097e7861bad1f53ba474f9a76c0b265a84bbb74b
wbxabcdefg <60501126+wbxabcdefg@users.noreply.github.com>
2022-03-23 09:12:44 +0800

    Add files via upload

---
+++ 韦冰雪ZS218276.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+
+# 没有列名进行重命名
+new_columns = ["user_id", "event_id"]
+df = pd.read_csv('F:/韦冰雪/研一下/大数据与云计算/0322/user_event.csv', names=new_columns, squeeze=True)
+# print(df)
+df50=df['user_id'].value_counts().head(50)
+print(df50)
+
+
